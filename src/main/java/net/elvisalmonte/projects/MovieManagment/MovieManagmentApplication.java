21f923e3a13bf8f3c2f3cdd9789054a618d736a1
Elvis Almonte <elvisalmonte14@gmail.com>
2025-01-26 14:02:09 -0400

    Creacion del proyecto MovieManagment

---
+++ src/main/java/net/elvisalmonte/projects/MovieManagment/MovieManagmentApplication.java
@@ -0,0 +1,14 @@
+package net.elvisalmonte.projects.MovieManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagmentApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MovieManagmentApplication.class, args);
+	}
+
+}
